e0feea5f38a270fec62449f4411ac43b26a5c252
RoshiniH06 <roshini0624@gmail.com>
2026-09-17 15:07:29 +0530

    initial

---
+++ Rose3.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("pinkBtn");
+
+  button.addEventListener("click", () => {
+    alert("Button clicked successfully!");
+  });
+});
