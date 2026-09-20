7d0518ae08bca8dd7d2320f1e82a356d529d45b2
RoshiniH06 <roshini0624@gmail.com>
2026-09-20 23:56:09 +0530

    First stage

---
+++ hey.js
@@ -0,0 +1,16 @@
+let studentName = "Roshini";
+let marks = 80;
+let age = 19;
+let course = "BCA";
+marks +=5;
+if (marks>=40)
+{console.log("greater or equal to 40");
+}
+if (age>=18)
+{console.log("greater or equal to 18");
+}
+console.log(studentName);
+console.log(marks);
+console.log(age);
+console.log(course);
+
