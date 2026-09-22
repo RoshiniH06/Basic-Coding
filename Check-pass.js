e7c32905257faf7d77cbb5342cbda4eb90d6aa78
RoshiniH06 <roshini0624@gmail.com>
2026-09-22 15:01:45 +0530

    day-4

---
+++ Check-pass.js
@@ -0,0 +1,10 @@
+let expectedUsername = "admin";
+let expectedPassword = "password123";
+let username = "admin"; 
+let password = "password123"; 
+if (username === expectedUsername && password === expectedPassword){
+    console.log("Login successful");
+} 
+else{
+    console.log("Login failed");
+}
